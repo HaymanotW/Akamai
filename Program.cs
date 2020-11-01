c5d2166c8d83ed253df1b8a2f7d85e6b16369808
Haymanot Zemene <5lettersehksy@gmail.com>
2020-11-01 12:36:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWebDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C://Users/yidne/Downloads/Compressed/chromedriver_win32");
+            driver.Url = "http://akamaijobs.referrals.selectminds.com/";
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+
+
+            driver.FindElement(By.Id("keyword")).SendKeys("Test");
+            IWebElement option = driver.FindElement(By.XPath("//*[@id='location_facet_chzn']")); // Option is visible and ok to use
+            driver.FindElement(By.XPath("//*[@id='location_facet_chzn_c_12']")).Click(); // Dropdown closing hides the option
+            option.Click(); // This option is no longer iteractable
+            driver.FindElement(By.XPath("//*[@id='location_facet_chzn']")).SendKeys("PL-Krakow");
+            driver.FindElement(By.Id("jSearchSubmit")).Click();
+        }
+    }
+}
---
+++ SeleniumTE.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumWebDriver
+{
+    class SeleniumTE
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C://Users/yidne/Downloads/Compressed/chromedriver_win32");
+        }
+
+        [Test]
+        public void test()
+        {
+            //keyword
+            //jSearchSubmit
+            driver.Url = "http://akamaijobs.referrals.selectminds.com";
+            driver.FindElement(By.Id("keyword")).Clear();
+            driver.FindElement(By.Id("keyword")).Click();
+            driver.FindElement(By.Id("keyword")).SendKeys("Test");
+            //driver.FindElement(By.XPath("//input[@id='jSearchSubmit']")).Click();
+            driver.FindElement(By.Id("jSearchSubmit")).Click();
+
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
